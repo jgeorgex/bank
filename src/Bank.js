3496f2e71dd5985b15c07813daf2f3d6fb60ebc0
Julian George <jules104@gmail.com>
2020-01-19 16:35:24 +0000

    Refactor remove default code and rename Bank src file

---
+++ src/Bank.js
@@ -0,0 +1,7 @@
+function BankAccount() {
+  BankAccount.prototype.deposit = function() {}; // end of deposit prototype
+
+  BankAccount.prototype.balance = function() {
+    return 1000;
+  }; // end of balance prototype
+} // end of BankAccount Function
